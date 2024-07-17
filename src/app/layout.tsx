9177390dd76ecfa4305217e62992f24fbae6f7a9
Eric Trimble <eric.trimo@gmail.com>
2024-07-17 11:33:18 -0400

    Add vercel speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { CartProvider } from './context/CartContext';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,6 +14,7 @@ export const metadata: Metadata = {
 export default function RootLayout({children}: Readonly<{children: React.ReactNode}>) {
   return (
     <html lang="en">
+      <SpeedInsights />
       <body className={inter.className}>
         <CartProvider>
           {children}
